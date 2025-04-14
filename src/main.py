94392d56dcd64834be2c7399d2fa572b4b311700
CALCATERRA <91848379+CALCATERRA@users.noreply.github.com>
2025-04-14 11:59:31 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def create_payment_link(chat_id, amount):
             {
                 "amount": {"currency_code": "EUR", "value": str(amount)},
                 "custom_id": str(chat_id),
-                "notify_url": "https://67f6d3471e1e1546c937.appwrite.global/v1/functions/67f6d345003e6da67d40/executions"
+                "notify_url": "https://67fcdabe296a2f5dcc1e.appwrite.global/v1/functions/67fcdc524135057660a9/executions"
             }
         ],
         "application_context": {
