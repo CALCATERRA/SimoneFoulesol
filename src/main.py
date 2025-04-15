bb0f7aa3ea7251c79d732a88c3a2c5785cb16076
CALCATERRA <91848379+CALCATERRA@users.noreply.github.com>
2025-04-15 14:39:43 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import requests
 
 # Config
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-PHOTO_URL = "https://i.imgur.com/tIcTyUK.jpg"
+PHOTO_URL = "https://cloud.appwrite.io/v1/storage/buckets/67f694430030364ac183/files/67f694ed0029e4957b1c/view?project=67f037f300060437d16d&mode=admin"
 PAYPAL_CLIENT_ID = os.environ.get("PAYPAL_CLIENT_ID")
 PAYPAL_SECRET = os.environ.get("PAYPAL_SECRET")
 
